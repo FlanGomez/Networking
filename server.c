1fe47a851e64e4b57a8448e20481082a2ad2860a
Flan Gomez <flanoygomes3@gmail.com>
2026-04-17 15:24:17 +0530

    Add Epoll server
    Basic-Intermediate Epoll server to handle multiple clients

--- server.c
+++ server.c
@@ -1,101 +1,183 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+#define PORT "8080"
+#define MAX_QUEUE 100
+#define MAX_TRY 20
+#define DNS NULL
+
+int client_group[100];
+int client_count = 0;
+
+void non_blocking(int fd){
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
 
 
 int main(void){
 
-    int server,client;
-    char message[1024];
+    //===== Variables ====//
+    int server_fd, client_fd;
+    int status = 0, epoll_fd, ready_fds;
+    char buffer[1024] = {0};
 
-    struct addrinfo req, *accessing_struct;
-    // ============ addrinfo ============ //
-    // addrinfo is a 48 byte in-build struct to hold 8 in-built variables
-    // 3 variables are on heap and rest 6 are on stack and we use only 1 variable from the 3 variables which are on heap which is ai_addr(which is sockaddr_in)
-    //
-    // ------ 8 variables ------
-    // ai_flags  => 4 byte
-    // ai_socktype => 4byte
-    // ai_protocol => 4byte
-    // ai_family  => 4byte
+    struct sockaddr_storage client_storage;
+    socklen_t len = sizeof(client_storage);
+    struct addrinfo req,*access;
+    //======== addrinfo ========//
+    // ai_flags  => 4bytes
+    // ai_family => 4bytes
+    // ai_protocol => 4bytes
+    // ai_socktype => 4bytes
     // ai_addrlen => 4byte
-    // struct ai_addr( The Heap allocation) => 8byte
-    // ai_canonname (Heap allocation) => 8byte
-    // ai_next (Heap allocation) => 8byte
-    // paddint  => 4 byte
-    //
-    // Total = 48 bytes
-    //
-    // ============ addrinfo ============ //
+    // ai_addr  ==> 8byte
+    // ai_canonname ==> 8byte
+    // ai_next  ==> 8byte
+    //==========================//
 
-    struct sockaddr_storage client_storage;  // This hold 128 bytes of storage to hold any IPv4 or IPv6
+    memset(&req,0,sizeof(req));  // Set the addrinfo struct members values to zero and later assigned them
+    req.ai_flags = AI_PASSIVE;
+    req.ai_family = AF_UNSPEC;
+    req.ai_protocol = 0;
+    req.ai_socktype = SOCK_STREAM;
 
-    socklen_t memory_space_of_client_storage = sizeof(client_storage);
-
-    memset(&req,0,sizeof(req)); // Set each variable in the struct as zero so example ai_flags = 0; because they are set to garbage values so we set them to zero
-    req.ai_family = AF_UNSPEC;  // (saying : I want any IPv4 or IPv6) IP
-    req.ai_socktype = SOCK_STREAM; // TCP or UDP
-    req.ai_flags = AI_PASSIVE;    // Receives any address WIFI ETHERNET OR ANY ADDRESS
-
-
-    // getaddrinfo is used to set the address and let the code be ready with all address and domains and ports
-    int status = getaddrinfo(0,"8000",&req,&accessing_struct); // ( host   port   stack_variable  heap_variable)
-    // dns is 0 cause this is a server
-    // port is set to 8000
-    // &req is the struct variable which has all the access to internal varibale of addrinfo and getaddrinfo automatically fills the all addresses by itself based on the IP SOCKTYPE n all etc
-    //&accessing_struct is used to create a heap by internal addrinfo struct 's variable ai_addr which create a heap to store the auto filled address and let it be used further
-    //ai_addr is sockaddr_in which is of interally 16 bytes which has IP, port, address of the server 127.0.0.1 and padding but in here we dont include padding so it is 8 byte
-
-    // Q1. Why &accessing_struct used in below code ?
-    // Because we need the sockaddr_in to do the further code which is just 16 byte packet which holds IP PORT ADDRESS
     
-    if(status != 0){
-        // if status any other number then 0 perform this
-        fprintf(stderr,"getaddrinfo : %s\n",gai_strerror(status)); // gai_strerror converts numbers to human readable forms
+    //====== Explaintion =====//
+    // This code converts the human readable lines to binary assigning post host and the whole addrinfo and also req data transfer to access
+    
+    for(int try = 0;try<MAX_TRY;try++){
+        status = getaddrinfo(DNS,PORT,&req,&access);
+        if(status == 0){
+            printf("\nSTATUS CODE : 200");
+            break;
+        }
+
+        else if(status == EAI_AGAIN){
+            printf("\nTRYING AGAIN....");
+            sleep(2);
+            continue;
+        }
+
+        else if(status != 0){
+            fprintf(stderr,"\ngetaddrinfo :%s\n",gai_strerror(status));
+            return 1;
+        }
+    }
+
+    
+    server_fd = socket(access -> ai_family,access -> ai_socktype,access -> ai_protocol);
+    if(server_fd == -1){
+        perror("\nSocket Failed");
         return 1;
     }
 
-    // Socket(server , IP, TCP/USP , PROTOCOL)
-    server = socket( accessing_struct -> ai_family, accessing_struct -> ai_socktype, accessing_struct -> ai_protocol);
-    if(server == -1){
-        printf("\nSocket Failed");
-        close(server);
+    int enable = 1;
+    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&enable,sizeof(enable)) == -1){
+        printf("\nSETSOCKOPT FAILED");
+        //exit(EXIT_FAILURE);
+        return 1;
+    }
+    
+
+    if(bind(server_fd,access -> ai_addr,access -> ai_addrlen) == -1){
+        if(errno == EADDRINUSE){
+            printf("\nPORT IN USE....");
+            return 1;
+        }
+        else if(errno == EBADF){
+            printf("\nFD ERROR");
+            return 1;
+        }
+        else{
+            perror("\nBind Failed");
+            return 1;
+        }
+    }
+    freeaddrinfo(access);
+
+    if(listen(server_fd,MAX_QUEUE) == -1){
+        perror("\nListen Failed");
     }
 
-    // Bind(server , sockaddr_in , sizeof(sockaddr_in)) or Bind(server , sockaddr , sizeof(sockaddr))
-    if(bind(server,accessing_struct -> ai_addr,accessing_struct -> ai_addrlen) == -1){
-        printf("\nBind Failed");
-        close(server);
-    }
-
-    // Freed the heap after making the server and binding the addresses
-    freeaddrinfo(accessing_struct);
-
-    if(listen(server,3) == -1){
-        printf("\nListen Failed");
-    }
-
-    // Client( server , sockaddr , &client_storage, sizeof(client_storage))
-    client = accept(server,(struct sockaddr *)&client_storage,&memory_space_of_client_storage);
-    if(client == -1){
-        printf("\nAccept Failed");
-        close(server);
-    }
-
-    while (1)
+    epoll_fd = epoll_create1(0);
+    if(epoll_fd == -1)
     {
-        memset(&message,0,sizeof(message));
-        printf("\nMessage : ");
-        fgets(message,sizeof(message),stdin);
-        write(client,message,strlen(message));
-
-        read(client,message,sizeof(message));
-        printf("\nClient : %s\n",message);
+        perror("\nEPOLL INSTANCE FAILED");
+        exit(EXIT_FAILURE);
     }
 
-    close(server);
+    struct epoll_event ev , ready_list[100];
+    ev.events = EPOLLIN;
+    ev.data.fd = server_fd;
+    if(epoll_ctl(epoll_fd,EPOLL_CTL_ADD,server_fd,&ev) == -1)
+    {
+        perror("\nEPOLL EVENT FAILED");
+        exit(EXIT_FAILURE);
+    }
+    non_blocking(server_fd);
 
+
+    while(1)
+    {
+        ready_fds = epoll_wait(epoll_fd,ready_list,100,-1);
+        if(ready_fds == -1){
+            perror("\nEPOLL WAIT FAILED");
+            exit(EXIT_FAILURE);
+        }
+
+
+        if(ready_fds > 0){
+            for(int each_ID = 0;each_ID < ready_fds; each_ID++){
+                int check_serverFD = ready_list[each_ID].data.fd;
+                if(check_serverFD == server_fd){
+                    client_fd = accept(server_fd,(struct sockaddr *)&client_storage,&len);
+                    if(client_fd == -1)
+                {
+                    perror("\nACCEPT FAILED");
+                    exit(EXIT_FAILURE);
+                }
+                
+                struct epoll_event watch;
+                watch.events = EPOLLIN;
+                watch.data.fd = client_fd;
+                epoll_ctl(epoll_fd,EPOLL_CTL_ADD,client_fd,&watch);
+                non_blocking(client_fd);
+
+                client_group[client_count] = client_fd;
+                printf("\nCLIENT ADDED");
+                client_count++;
+            }
+
+                else{
+                    memset(buffer,0,sizeof(buffer));
+                    int bytes = read(check_serverFD,buffer,sizeof(buffer));
+                    if(bytes > 0){
+                        for(int check_ID = 0;check_ID < client_count; check_ID++){
+                            if(client_group[check_ID] != check_serverFD){
+                                write(client_group[check_ID],buffer,bytes);
+                            }
+                        }
+                        printf("\nClient : %s\n",buffer);
+                    }
+                }
+            }
+        }
+    }
 }
+
+
+
+
+
+
